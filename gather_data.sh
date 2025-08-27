e2b6509970cda2a180d4a375aba5fd2882b878d2
Javier Castro <javier@elementalbasis.com>
2025-08-26 21:11:12 -0400

    Modified `gather_data.sh` to have more reasonable for loop bounds.

--- gather_data.sh
+++ gather_data.sh
@@ -10,11 +10,11 @@ max_target_uncertainty=0.01
 min_threshold=100
 max_threshold=10000
 
-for i in {1..100}; do
+for i in {1..10}; do
 	sample_size=$(shuf -i $min_sample_size-$max_sample_size -n 1)
-	for j in {1..100}; do
+	for j in {1..10}; do
 		target_uncertainty=$(python -c "import random; print(random.uniform($min_target_uncertainty, $max_target_uncertainty))")
-		for k in {1..100}; do
+		for k in {1..10}; do
 			threshold=$(shuf -i $min_threshold-$max_threshold -n 1)
 			julia -p $ncores $filename --sample-size $sample_size --target-uncertainty $target_uncertainty --threshold $threshold
 		done
